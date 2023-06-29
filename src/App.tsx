9cfee49d909b35e7247d1beab3860b6261a04762
wuliheng <1245183029@qq.com>
2023-06-29 16:21:54 +0800

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="text-lg">番茄时钟</div>
+    </div>
+  );
+}
+
+export default App;
