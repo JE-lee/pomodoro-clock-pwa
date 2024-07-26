307594a92be45b5f21440cbb9f1bfd5009872906
Maurice <liheng.wu77@gmail.com>
2024-07-26 13:52:03 +0800

    feat: optimize animation

--- src/components/PomodoroClock/index.tsx
+++ src/components/PomodoroClock/index.tsx
@@ -1,12 +1,12 @@
 import { useCallback, useContext, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import type { FC } from 'react'
+import { Flipped, Flipper } from 'react-flip-toolkit'
+import { twMerge } from 'tailwind-merge'
 import { formatMinAndSec, isEqualDate, noThrow } from '../../shared'
 import { STATE, ThreadType } from '../../type'
 import { PauseIcon, PlayIcon, ResetIcon, SkipIcon } from '../SvgIcon'
 import { ClockContext, HeatMapContext, addThread, getThreadsOfDay, useCountdown, useNotification } from '../../service'
 import NumberInput from '../NumberInput'
-import Flip from '../Flip'
-import { useFade } from '../../hooks'
 
 interface PomodoroClockProps {
   clockState: STATE
@@ -19,14 +19,12 @@ const PomodoroClock: FC<PomodoroClockProps> = (props) => {
   const [sessionRound, setSessionRound] = useState(1)
   const [breakRound, setBreakRound] = useState(1)
   const [restSeconds, setRestSeconds] = useState(sessionTime * 60)
-  const [clockFlipTrigger, setClockFlipTrigger] = useState(false)
   const shouldShowNotiPermissionModal = useRef(true)
   const inSession = isInSession(props.clockState)
-  const currentDate = useRef<Date>()
-  if (!currentDate.current)
-    currentDate.current = new Date()
+  const currentDate = useRef<Date>(new Date())
+  const controlsRef = useRef<HTMLDivElement>(null)
+  const controlsRect = useRef<DOMRect>()
 
-  const { elRef: controlsRef, fadeIn: fadeControlsIn, fadeOut: fadeControlsOut } = useFade()
   const threadStartTimestamp = useRef(Date.now())
 
   const { confirm, closeNotification } = useNotification()
@@ -100,13 +98,8 @@ const PomodoroClock: FC<PomodoroClockProps> = (props) => {
   }, [props.clockState, sessionTime, breakTime, sessionRound, breakRound, silent, sessionHints, breakHints])
 
   useLayoutEffect(() => {
-    if (inSession)
-      fadeControlsOut(400)
-    else
-      fadeControlsIn(800)
-
-    // must be called after fadeControlsOut/In
-    setClockFlipTrigger(!clockFlipTrigger)
+    if (!inSession && !controlsRect.current)
+      controlsRect.current = controlsRef.current?.getBoundingClientRect()
   }, [inSession])
 
   const onSesionTimeChange = useCallback((value: number) => {
@@ -203,82 +196,122 @@ const PomodoroClock: FC<PomodoroClockProps> = (props) => {
     stateText = inSession ? `Break ${breakRound}` : 'Resting'
 
   return (
-    <div className="flex flex-col items-center select-none">
-      <Flip trigger={clockFlipTrigger}>
-        <div className="w-[55vmin] min-w-[360px] aspect-square min-h-[360px]
-            flex flex-col justify-center items-center bg-pomodoro bg-center bg-no-repeat bg-contain text-xs sm:text-sm xl:text-base 2xl:text-lg">
-          <div className="text-[2.2em] leading-tight text-[#FD7477]">{stateText}</div>
-          <div className="text-[4em] leading-none font-semibold text-[#FC5E7B]">{formatMinAndSec(restSeconds)}</div>
-          { props.clockState === STATE.RUNNING
-            && <button className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
-              onClick={doPauseSession}>
-              <PauseIcon className="w-3 mr-1"></PauseIcon>
-              <span>PAUSE</span>
-            </button>
-          }
-          { (props.clockState === STATE.BEFORE_RUN || props.clockState === STATE.PAUSED)
-              && <button className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
-              onClick={() => doStartSession(props.clockState === STATE.BEFORE_RUN)}>
-              <PlayIcon className="w-3 mr-1"></PlayIcon>
-              <span>START</span>
-            </button>
-          }
-          { (props.clockState === STATE.BEFORE_BREAK)
-              && <button className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
-              onClick={doStartBreak}>
-              <PlayIcon className="w-3 mr-1"></PlayIcon>
-              <span>START</span>
-            </button>
-          }
-          { (props.clockState === STATE.BREAKING)
-              && <button className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
-              onClick={doSkipBreak}>
-              <SkipIcon className="w-5 mr-1"></SkipIcon>
-              <span>SKIP</span>
-            </button>
-          }
-          {props.clockState === STATE.PAUSED
-              && <button className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
-                  onClick={onResetClick}>
-              <ResetIcon className="w-3 mr-1"></ResetIcon>
-              <span>Reset</span>
-            </button>
-          }
-        </div>
-      </Flip>
-      {/* controls */}
-      {
-        <div ref={controlsRef} className="flex items-center mt-8">
-          <NumberInput label="SESSION" value={sessionTime}
-          onChange={onSesionTimeChange} min={0}></NumberInput>
-          <NumberInput label="BREAK" className="ml-5" value={breakTime} onChange={onBreakTimeChange} min={0}></NumberInput>
-        </div>
-      }
-      {
-        <dialog id="notiPermissionModal" className="modal">
-          <form method="dialog" className="modal-box">
-            <button className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">x</button>
-            <h3 className="text-lg font-bold">Hello</h3>
-            <p className="py-2">
-              {'The app requires you to authorize your browser\'s notification permissions for an optimal experience!'}
-            </p>
-            <p className="py-2">
+    <Flipper flipKey={inSession}>
+      <div className="flex flex-col items-center select-none">
+        <Flipped flipId="clock">
+          <div
+            className="w-[55vmin] min-w-[360px] aspect-square min-h-[360px]
+                flex flex-col justify-center items-center bg-pomodoro bg-center bg-no-repeat bg-contain text-xs sm:text-sm xl:text-base 2xl:text-lg"
+          >
+            <div className="text-[2.2em] leading-tight text-[#FD7477]">{stateText}</div>
+            <div className="text-[4em] leading-none font-semibold text-[#FC5E7B]">{formatMinAndSec(restSeconds)}</div>
+            {props.clockState === STATE.RUNNING && (
+              <button
+                className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
+                onClick={doPauseSession}
+              >
+                <PauseIcon className="w-3 mr-1"></PauseIcon>
+                <span>PAUSE</span>
+              </button>
+            )}
+            {(props.clockState === STATE.BEFORE_RUN || props.clockState === STATE.PAUSED) && (
+              <button
+                className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
+                onClick={() => doStartSession(props.clockState === STATE.BEFORE_RUN)}
+              >
+                <PlayIcon className="w-3 mr-1"></PlayIcon>
+                <span>START</span>
+              </button>
+            )}
+            {props.clockState === STATE.BEFORE_BREAK && (
+              <button
+                className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
+                onClick={doStartBreak}
+              >
+                <PlayIcon className="w-3 mr-1"></PlayIcon>
+                <span>START</span>
+              </button>
+            )}
+            {props.clockState === STATE.BREAKING && (
+              <button
+                className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
+                onClick={doSkipBreak}
+              >
+                <SkipIcon className="w-5 mr-1"></SkipIcon>
+                <span>SKIP</span>
+              </button>
+            )}
+            {props.clockState === STATE.PAUSED && (
+              <button
+                className="flex items-center text-[1.4em] leading-tight font-simibold text-[#FC5E7B] cursor-pointer"
+                onClick={onResetClick}
+              >
+                <ResetIcon className="w-3 mr-1"></ResetIcon>
+                <span>Reset</span>
+              </button>
+            )}
+          </div>
+        </Flipped>
+        {/* controls */}
+        <Flipped
+          flipId="control"
+          onAppear={(el) => {
+            const animation = el.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 400, easing: 'ease' })
+            animation.onfinish = () => el.style.opacity = '1'
+          }}
+          onExit={(el, index, removeElement) => {
+            const { x: px, y: py } = controlsRect.current || {}
+            if (!px || !py) {
+              removeElement()
+              return
+            }
+
+            const { x: cx, y: cy } = el.getBoundingClientRect()
+            const xOffset = px - cx
+            const yOffset = py - cy
+            el.style.transform = `translate(${xOffset}px, ${yOffset}px)`
+            const animation = el.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 400, easing: 'ease' })
+            animation.onfinish = removeElement
+          }}
+        >
+          {!inSession && (
+            <div ref={controlsRef} className={twMerge('flex items-center mt-8')}>
+              <NumberInput label="SESSION" value={sessionTime} onChange={onSesionTimeChange} min={0}></NumberInput>
+              <NumberInput
+                label="BREAK"
+                className="ml-5"
+                value={breakTime}
+                onChange={onBreakTimeChange}
+                min={0}
+              ></NumberInput>
+            </div>
+          )}
+        </Flipped>
+        {
+          <dialog id="notiPermissionModal" className="modal">
+            <form method="dialog" className="modal-box">
+              <button className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">x</button>
+              <h3 className="text-lg font-bold">Hello</h3>
+              <p className="py-2">
+                {'The app requires you to authorize your browser\'s notification permissions for an optimal experience!'}
+              </p>
+              <p className="py-2">
               Please click the button in the upper left corner of your browser to re-authorize, or check out
               &nbsp;<a className="link" href="https://support.google.com/chrome/answer/3220216?hl=en&co=GENIE.Platform%3DDesktop" target="_blank" rel="noreferrer">this page</a> for help.
               &nbsp;if you are using mac, look at  <a className="link" target="_blank" href="https://support.apple.com/en-hk/guide/safari/sfri40734/mac" rel="noreferrer">this website</a> for help.
-            </p>
-            <div className="modal-action">
+              </p>
+              <div className="modal-action">
               { Notification.permission === 'default' && <button className="btn btn-md text-white bg-[#FC5E7B]" onClick={() => Notification.requestPermission()}>AUTHORIZE</button>}
               <button className="btn btn-md" onClick={() => (shouldShowNotiPermissionModal.current = false)}>No more reminders</button>
-            </div>
-          </form>
-          <form method="dialog" className="modal-backdrop">
-            <button>close</button>
-          </form>
-
-        </dialog>
-      }
-    </div>
+              </div>
+            </form>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
+        }
+      </div>
+    </Flipper>
   )
 }
 
